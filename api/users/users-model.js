9dc3ec5d3e69787995dda8519b817372d8b77784
Amare Shakur <amare-shakur@lambdastudents.com>
2021-04-20 02:26:51 -0400

    pushed

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,8 +1,13 @@
-/**
+const dbConfig = require("../../data/db-config");
+
+module.exports = {
+  find,findById,findBy,add}
+
+  /**
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-
+return db('users');
 }
 
 /**
@@ -16,14 +21,17 @@ function findBy(filter) {
   resolves to the user { user_id, username } with the given user_id
  */
 function findById(user_id) {
-
+return db('users').where({ user_id }).first();
 }
 
 /**
   resolves to the newly inserted user { user_id, username }
  */
 function add(user) {
-
+db('users').insert(user)
+.then(ids => {
+  return findById(ids[0]);
+});
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
